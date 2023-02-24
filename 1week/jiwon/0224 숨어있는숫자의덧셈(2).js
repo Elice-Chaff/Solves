8e7ea001e070b350d21eee327f3ae8ef172d0f01
[AI6/1반] 최지원 <qpdlqltb1215@gmail.com>
2023-02-24 23:24:21 +0900

    최지원: 0224 문제풀이

---
+++ 1week/jiwon/0224 숨어있는숫자의덧셈(2).js
@@ -0,0 +1,16 @@
+// Lv 0. 숨어있는 숫자의 덧셈
+// https://school.programmers.co.kr/learn/courses/30/lessons/120864
+
+function solution(my_string) {
+  let answer = 0
+  for (let i = 0; i < my_string.length; i++) {
+      let num = '' //1의자리 이상일때 숫자추가해서 하나의 숫자 만들기위해
+      while (Number.isInteger(Number(my_string[i]))) { //정수면
+          num += Number(my_string[i]) //숫자추가하기
+          i++ //다음인덱스도 정수인지 확인해야하니까
+      }
+      answer += Number(num)
+      // console.log(answer)
+  } 
+  return answer
+}
