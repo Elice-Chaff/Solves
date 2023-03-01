cc9470851ba673ba5173a3621662e093482e7055
JiHwan <hg_07@naver.com>
2023-03-02 02:00:36 +0900

    jihwan: 캐릭터 좌표 ref

--- 2week/jihwan/0301_캐릭터의좌표.js
+++ 2week/jihwan/0301_캐릭터의좌표.js
@@ -1,27 +1,25 @@
-//  프로그래머스 level 0 캐릭터의 자표
-// https://school.programmers.co.kr/learn/courses/30/lessons/120861
-
 function solution(keyinput, board) {
   // 배열에 두개의 값 리턴..
-  // 각 케이스마다
+  // 각 케이스마다 , left, down은 음수기 때문에 board도 음수로 변환 비교
 
-  var answer = [0, 0];
+  let answer = [0, 0];
 
-  let downVelify = -Math.floor(board[1] / 2);
-  let upVelify = Math.floor(board[1] / 2);
-  let leftVelify = -Math.floor(board[0] / 2);
-  let rightVelify = Math.floor(board[0] / 2);
+  let heightVelify = Math.floor(board[1] / 2);
+  let widthVelify = Math.floor(board[0] / 2);
 
   for (i in keyinput) {
-    if (keyinput[i] == "left" && leftVelify != answer[0]) {
+    if (keyinput[i] == "left" && -widthVelify < answer[0]) {
       answer[0]--;
-    } else if (keyinput[i] == "right" && rightVelify != answer[0]) {
+    } else if (keyinput[i] == "right" && widthVelify > answer[0]) {
       answer[0]++;
-    } else if (keyinput[i] == "up" && upVelify != answer[1]) {
+    } else if (keyinput[i] == "up" && heightVelify > answer[1]) {
       answer[1]++;
-    } else if (keyinput[i] == "down" && downVelify != answer[1]) {
+    } else if (keyinput[i] == "down" && -heightVelify < answer[1]) {
       answer[1]--;
     }
   }
+
   return answer;
 }
+
+// Switch로 바꿔보기
